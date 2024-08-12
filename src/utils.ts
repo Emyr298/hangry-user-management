b30ecc0c9f2846155460a071fdfd6634a84d263f
Emyr298 <emi.ru298@gmail.com>
2024-08-12 18:05:08 +0700

    feat: implement get body

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import http from 'http';
+
 // Separates query string from path in req.url
 export const extractPath = (url: string) => {
   const urlObj = new URL("https://placehoder" + url);
@@ -6,4 +8,23 @@ export const extractPath = (url: string) => {
     ret = ret.slice(0, ret.length - 1);
   }
   return ret;
+};
+
+function getRequestBody(req: http.IncomingMessage): Promise<string> {
+  return new Promise((resolve, reject) => {
+    let body = '';
+    req.on('data', (chunk) => {
+      body += chunk.toString();
+    });
+    req.on('end', () => {
+      resolve(body);
+    });
+    req.on('error', (err) => {
+      reject(err);
+    })
+  });
+};
+
+export const getJsonBody = async (req: http.IncomingMessage) => {
+  return await getRequestBody(req);
 };
