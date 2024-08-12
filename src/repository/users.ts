a58b5ed710db9f8b79f4f20b973e9ddc40ecb6b1
Emyr298 <emi.ru298@gmail.com>
2024-08-12 16:30:12 +0700

    feat: implement user repository

---
+++ src/repository/users.ts
@@ -0,0 +1,37 @@
+import { UserForm } from "../models/forms/user-form";
+import { User } from "../models/user";
+import { v4 as uuid } from "uuid";
+
+const users: User[] = []
+
+export const getAllUsers = () => {
+  return users;
+};
+
+export const getUserById = (userId: string) => {
+  const user = users.find((user) => {
+    return user.id === userId;
+  })
+  if (!user) {
+    throw new Error("user not found");
+  }
+  return user;
+};
+
+export const createUser = (user: UserForm) => {
+  const id = uuid()
+  users.push({
+    id: id,
+    ...user
+  });
+};
+
+export const deleteUser = (userId: string) => {
+  const userIdx = users.findIndex((user) => {
+    return user.id === userId;
+  });
+  if (userIdx < 0) {
+    throw new Error("user not found");
+  }
+  users.splice(userIdx, 1);
+};
