19b191797c4a58194035b9e15f14495f7d8fb187
Emyr298 <emi.ru298@gmail.com>
2024-08-12 14:26:59 +0700

    init: eslint and prettier

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
-import http from "http";
+import http from 'http';
 
 const port = 3000;
 const server = http.createServer((req, res) => {
-  res.end("Hello, World!");
+  res.end('Hello, World!');
 });
 
 server.listen(port, () => {
